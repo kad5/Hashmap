73f53bd6c0beb137f8e5cfeed6117814f8820db4
kad5 <khaled.mohamed.1807@gmail.com>
2025-01-13 22:43:42 +0200

    all core methods done -  move to testing

--- hashmap.js
+++ hashmap.js
@@ -4,7 +4,7 @@ if (index < 0 || index >= buckets.length) {
 }*/
 
 class HashMap {
-  constructor(capacity) {
+  constructor(capacity = 16) {
     this.map = [];
     this.capacity = capacity;
     this.loadFactor = this.length() / this.capacity;
@@ -12,7 +12,6 @@ class HashMap {
     // call initMap automatically when a new object is created
     this.initMap();
   }
-
   // initMap:
   // if the map is empty, it fills its capacity with nulls and early returns since
   // length() would be zero. if the map has items it creates a shallow copy and
@@ -26,7 +25,6 @@ class HashMap {
   // we dont have to reset the nextNode to null since we are taking the key and
   // value and creating entire new linked lists via out set method.
   // lastly reset the isBeingResized to allow resizing if needed.
-
   initMap() {
     this.isBeingResized = true;
     const hasItems = this.length();
@@ -51,16 +49,17 @@ class HashMap {
     });
     this.isBeingResized = false;
   }
-
   hash(key) {
     let hashCode = 0;
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
       hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
     }
+    if (Math.abs(hashCode) > this.capacity) {
+      throw new Error("Limit exceeded");
+    }
     return Math.abs(hashCode);
   }
-
   //set:
   // takes the key and value and creates a temp bucket with a hashKey. then if the
   // index corresponding to the  hashKey is empty it adds the bucket, if there is a index
@@ -70,7 +69,6 @@ class HashMap {
   // the value. if it doesnt then it adds it to the tail of the linked list [works also if
   // the linked list has only a head]. after each new bucket item is introduced, it recal-
   // culates the load factor and checks the load limits to intialize an expansion if needed
-
   set(key, value) {
     const bucket = { key, value, nextNode: null };
     const hashIndex = this.hash(key);
@@ -98,46 +96,150 @@ class HashMap {
       return;
     }
   }
-
+  //remove: hashes the key and checks if the index exists, if not it returns
+  // if there is a bucket then first it checks if the bucket head matches - no
+  // collision and that there isnt a linked list. if collision is the case with
+  // no linked list, it returns. if there is a linked list the we traverse in case
+  // non matching key. once a match is found, weather head or inside the list
+  // it is excluded and true returned.
+  remove(key) {
+    const hashIndex = this.hash(key);
+    let node = this.map[hashIndex];
+    if (node !== null) {
+      if (node.key !== key && node.nextNode === null) {
+        return false;
+      }
+      if (node.key === key && node.nextNode === null) {
+        this.map[hashIndex] = null;
+        return true;
+      }
+      if (node.key === key && node.nextNode !== null) {
+        this.map[hashIndex] = node.nextNode;
+        return true;
+      }
+      let previousNode = node;
+      node = previousNode.nextNode;
+      while (node !== null) {
+        if (node.key === key) {
+          previousNode.nextNode = node.nextNode || null;
+          return true;
+        }
+        previousNode = node;
+        node = previousNode.nextNode;
+      }
+    }
+    return false;
+  }
   //checkload:
   // gets called after each item is added or removed. it first updates
   // the load factor. then only if there is no process of rehashing going
   // it proceeds to check if a rehash and expansion or shrinkage is needed
   // shrinkage can only happen if the limit above a base capacity of 16
-
   checkLoad() {
     this.loadFactor = this.length() / this.capacity;
     if (this.isBeingResized === true) return;
-    if (this.loadFactor >= 0.8) {
+    if (this.loadFactor >= 0.75) {
       this.capacity = this.capacity * 2;
       this.initMap();
       return;
     }
-    if (this.loadFactor <= 0.2 && this.capacity > 16) {
+    if (this.loadFactor <= 0.25 && this.capacity > 16) {
       this.capacity = this.capacity / 2;
       this.initMap();
     }
   }
+  // get checks all bucket heads and any linked lists found
+  get(key) {
+    const hashIndex = this.hash(key);
+    if (this.map[hashIndex]) {
+      if (this.map[hashIndex].key === key) {
+        return this.map[hashIndex].value;
+      }
+      let node = this.map[hashIndex];
+      while (node !== null) {
+        if (node.key === key) return node.value;
+        node = node.nextNode;
+      }
+    }
+    return null;
+  }
+  // has is a copy of get with just truthy and falsy returns
+  has(key) {
+    const hashIndex = this.hash(key);
+    if (this.map[hashIndex]) {
+      if (this.map[hashIndex].key === key) {
+        return true;
+      }
+      let node = this.map[hashIndex];
+      while (node !== null) {
+        if (node.key === key) return true;
+        node = node.nextNode;
+      }
+    }
+    return false;
+  }
 
-  get(key) {}
-  has(key) {}
-  remove(key) {}
-
+  // checks the number of all key value pairs (including in the linked list)
   length() {
     let count = 0;
     for (let i = 0; i < this.map.length; i++) {
-      if (this.map[i] !== null) count++;
+      if (this.map[i] !== null) {
+        if (this.map[i].nextNode === null) {
+          count++;
+        }
+        if (this.map[i].nextNode !== null) {
+          let node = this.map[i];
+          while (node !== null) {
+            count++;
+            node = node.nextNode;
+          }
+        }
+      }
     }
     return count;
   }
-  clear() {
+  // clears the map and intializes a new one with a passed capacity or def 16
+  clear(capacity = 16) {
     this.map = [];
+    this.capacity = capacity;
+    this.loadFactor = 0;
+    this.isBeingResized = false;
     this.initMap();
-    return this.map;
   }
-  keys() {}
-  values() {}
-  entries() {}
+  // gets all keys in an array whether heads or linked lists
+  keys() {
+    const allKeys = [];
+    this.map.forEach((bucket) => {
+      let node = bucket;
+      while (node !== null) {
+        allKeys.push(node.key);
+        node = node.nextNode;
+      }
+    });
+    return allKeys;
+  }
+  values() {
+    const allvalues = [];
+    this.map.forEach((bucket) => {
+      let node = bucket;
+      while (node !== null) {
+        allvalues.push(node.value);
+        node = node.nextNode;
+      }
+    });
+    return allvalues;
+  }
+  entries() {
+    const allEntries = [];
+    this.map.forEach((bucket) => {
+      let node = bucket;
+      while (node !== null) {
+        allEntries.push([node.key, node.value]);
+        node = node.nextNode;
+      }
+    });
+    return allEntries;
+  }
 }
 
 const myMap = hashmapGenerator();
