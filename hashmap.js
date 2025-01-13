fd0036a07260c77b5cb20422c8bc34b61e4b0315
kad5 <khaled.mohamed.1807@gmail.com>
2025-01-13 20:28:46 +0200

    made some progress to the map intiializationa nd handling

--- hashmap.js
+++ hashmap.js
@@ -11,10 +11,33 @@ class HashMap {
   }
 
   initMap() {
-    const updatedMap = [];
-    for (let i = 0; i < capacity; i++) {
-      updatedMap.push(null);
+    //create a shallow copy of our map
+    const shallowCopy = this.map.slice();
+    //create a new full capacity empty map
+    for (let i = 0; i < this.capacity; i++) {
+      this.map.push(null);
     }
+    //rehash all keys in the old buckets and spread them over the new map
+    shallowCopy.forEach((bucket) => {
+      // if the bucket has only 1 key value pair
+      if (bucket.nextNode === null) {
+        const newHashKey = this.hash(bucket.key);
+        this.set(newHashKey, bucket.value);
+      }
+      // if the bucket has a linked list, loop and hash each
+      if (bucket.nextNode !== null) {
+        let node = bucket;
+        while (node !== null) {
+          const newHashKey = this.hash(node.key);
+          this.set(newHashKey, node.value);
+          node = node.nextNode;
+          // we dont have to reset the nextNode to null since we are
+          // taking the key and value and creating entire new linked lists
+          // via out set method.
+        }
+      }
+    });
+
     for (let i = 0; i < this.map.length; i++) {
       updatedMap[i] = this.map[i];
     }
@@ -23,13 +46,11 @@ class HashMap {
 
   hash(key) {
     let hashCode = 0;
-
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
     }
-
-    return hashCode;
+    return Math.abs(hashCode);
   }
 
   set(key, value) {
@@ -38,6 +59,7 @@ class HashMap {
     // if the index is empty
     if (this.map[hashIndex] === null) {
       this.map[hashIndex] = bucket;
+
       return;
     }
     // to modify a value for an existing key at the head
@@ -54,8 +76,8 @@ class HashMap {
       if (tempNode.key && tempNode.key === key) {
         tempNode.value = value;
       }
-      if (tempNode === null) {
-        tempNode = bucket;
+      if (tempNode.nextNode === null) {
+        tempNode.nextNode = bucket;
       }
       return;
     }
